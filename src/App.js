4435e184fcf25e157c187f4a0f75e9473ca48819
Jitesh Pentakota <pentakota_jitesh.ghrcecs@raisoni.net>
2021-06-20 00:35:46 +0530

    added react-router and did page rendering

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import React from "react";
+import {Switch,Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div>
-      hello !!
-    </div>
+    <Switch>
+        <Route exact path="/">
+          This is working!
+        </Route>
+        <Route exact path="/1547">
+            This is working or not!
+        </Route>
+
+        <Route>
+            <h1>Page not Found!!</h1>
+        </Route>
+    </Switch>
   );
 }
 
